b57dd85251102f67f9073e5c5f8af1359c04ddc5
sebin kwon <sbkwon98@gmail.com>
2022-09-30 15:08:14 +0900

    Update detail and  Add delete, edit,update

--- pair/reviews/views.py
+++ pair/reviews/views.py
@@ -11,12 +11,12 @@ def index(request):
 def create(request):
     title = request.GET.get("title")
     content = request.GET.get("content")
-    print(title, content)
 
-    Review.objects.create(title=title, content=content)
+    review =  Review.objects.create(title=title, content=content)
+    review_pk = Review.objects.get(pk=review.pk)
     context = {"title": title, "content": content}
 
-    return redirect("reviews:detail")
+    return redirect("reviews:detail", review_pk.pk )
 
 
 def new(request):
@@ -27,3 +27,27 @@ def detail(request, pk):
     review = Review.objects.get(pk=pk)
     context = {"review": review}
     return render(request, "reviews/detail.html", context)
+
+def delete(request, pk):
+    Review.objects.get(pk=pk).delete()
+    return redirect("reviews:index")
+
+def edit(request, pk):
+    review = Review.objects.get(pk=pk)
+    context = {
+        "review" : review
+    }
+    return render(request, "reviews/edit.html", context)
+
+def update(request, pk):
+    review = Review.objects.get(pk=pk)
+
+    title_ = request.GET.get('title')
+    content_ = request.GET.get('content')
+
+    review.title = title_
+    review.content = content_
+
+    review.save()
+
+    return redirect("reviews:detail", review.pk)
